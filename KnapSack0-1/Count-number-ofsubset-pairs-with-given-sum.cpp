44d88a3e52c8640e9706a4b39eec3ab17180137a
Shivam Kumar <66867825+shivamsince0502@users.noreply.github.com>
2022-02-25 01:25:50 +0530

    Create Count-number-ofsubset-pairs-with-given-sum.cpp

---
+++ KnapSack0-1/Count-number-ofsubset-pairs-with-given-sum.cpp
@@ -0,0 +1,69 @@
+//https://www.cuplex2b.com/2021/01/number-of-subset-with-given-difference.html#:~:text=1.,diff%20be%20the%20given%20difference.&text=%3D%3E%20sum1%3D%20(diff%2B,same%20as%20subset%20sum%20problem.
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+
+{
+
+    int n,diff;
+
+    cin>>n>>diff;
+
+    int a[n];
+
+    for(int i=0;i<n;i++)
+
+        cin>>a[i];
+
+    int sum=0;
+
+    for(int i=0;i<n;i++)
+
+        sum=sum+a[i];
+
+    int sum1=(diff+sum)/2;
+
+    int dp[n+1][sum1+1];
+
+    for(int i=0;i<n+1;i++)
+
+    {
+
+        for(int j=0;j<sum1+1;j++)
+
+        {
+
+            if(j==0)
+
+                dp[i][j]=1;
+
+            else if(i==0)
+
+                dp[i][j]=0;
+
+            else
+
+            {
+
+                if(a[i-1]<=j)
+
+                    dp[i][j]=dp[i-1][j]+dp[i-1][j-a[i-1]];
+
+                else
+
+                    dp[i][j]=dp[i-1][j];
+
+            }
+
+        }
+
+    }
+
+   cout<<dp[n][sum1]<<endl;
+
+    return 0;
+
+
+}
